98df192915ef6dd4f5e98176842b926d01dd53aa
wannalearn123 <nurdiansyahagung277@gmail.com>
2026-09-08 23:59:04 +0700

    fix: zero topbar padding + Keluar button red

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -22,7 +22,7 @@
         <?php endif; ?>
         <form class="inline" method="post" action="/logout" title="<?= e($appUser['username'] ?? '') ?>">
           <?= csrf_field() ?>
-          <button class="navlink" type="submit">Keluar (<?= e($appUser['username'] ?? '') ?>)</button>
+          <button class="navlink danger" type="submit">Keluar (<?= e($appUser['username'] ?? '') ?>)</button>
         </form>
       <?php endif; ?>
     </nav>
